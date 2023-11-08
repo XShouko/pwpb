60793dc6a5611508a003a82dd997c9e029e15961
XShouko <96644544+XShouko@users.noreply.github.com>
2023-11-08 09:44:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ function changeSlide(n) {
         slide.style.transform = `translateX(-${slideIndex * 100}%)`;
     }
 }
+const productContainers = [...document.querySelectorAll('.product-container')];
+const productContainers1 = [...document.querySelectorAll('.product-container1')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+const nxtBtn1 = [...document.querySelectorAll('.nxt-btn1')];
+const preBtn1 = [...document.querySelectorAll('.pre-btn1')];
+
+  productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth; // Menggunakan scrollLeft untuk scroll ke kiri
+    })
+});
+
+productContainers1.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn1[i].addEventListener('click', () => {
+      item.scrollLeft += containerWidth;
+  })
+
+  preBtn1[i].addEventListener('click', () => {
+      item.scrollLeft -= containerWidth; // Menggunakan scrollLeft untuk scroll ke kiri
+  })
+});
