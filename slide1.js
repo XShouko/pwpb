9903c74639dba6fa3251a425df8fc3fe04b5d4a7
XShouko <96644544+XShouko@users.noreply.github.com>
2023-11-15 10:43:59 +0800

    Add files via upload

---
+++ slide1.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+
+function changeSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+    slideIndex += n;
+
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    } else if (slideIndex > slides.length - 1) {
+        slideIndex = 0;
+    }
+
+    for (let slide of slides) {
+        slide.style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+}
