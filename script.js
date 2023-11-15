93e2f6c23dfdf7a9e470fe38992e517e7db896fe
XShouko <96644544+XShouko@users.noreply.github.com>
2023-11-15 10:46:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -30,34 +30,3 @@ productContainers1.forEach((item, i) => {
         item.scrollLeft -= containerWidth; // Menggeser ke kiri dengan mengurangkan lebar container
     })
 });
-
-function changeSlide(n) {
-    let slides = document.getElementsByClassName("slide");
-    slideIndex += n;
-
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    } else if (slideIndex > slides.length - 1) {
-        slideIndex = 0;
-    }
-
-    for (let slide of slides) {
-        slide.style.transform = `translateX(-${slideIndex * 100}%)`;
-    }
-}
-let slideIndex = 0;
-
-function changeSlide(n) {
-    let slides = document.getElementsByClassName("geser");
-    slideIndex += n;
-
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    } else if (slideIndex > slides.length - 1) {
-        slideIndex = 0;
-    }
-
-    for (let slide of slides) {
-        slide.style.transform = `translateX(-${slideIndex * 100}%)`;
-    }
-}
